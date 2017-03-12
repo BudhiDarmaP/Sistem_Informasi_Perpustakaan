8c6cc9839688737ea0d9396b7e22978cd8fb4ee1
budhidarmap <budhidarmap@DESKTOP-8U7BB6O>
2017-03-12 12:55:25 +0700

    Search FIX Lanjut Peminjaman

--- src/java/Control/ControlPencarian.java
+++ src/java/Control/ControlPencarian.java
@@ -19,19 +19,6 @@ public class ControlPencarian extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ControlPencarian</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ControlPencarian at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
     }
 
     @Override
@@ -39,6 +26,12 @@ public class ControlPencarian extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
+             out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ControlPencarian</title>");
+            out.println("</head>");
+            out.println("<body>");
             processRequest(request, response);
             String key = request.getParameter("key");
             Buku[] bk = Buku.getListPencarian(key);
@@ -58,17 +51,19 @@ public class ControlPencarian extends HttpServlet {
                         + "<td>Tahun Terbit</td>"
                         + "<td>Ketersediaan</td></tr>");
                 for (int i = 0; i < bk.length; i++) {
-                    out.print("<tr><td>"+i+1
+                    out.print("<tr><td>"+(i+1)
                             + "<td>"+bk[i].getID_Buku()+"</td>"
                             + "<td>"+bk[i].getISBN()+"</td>"
                             + "<td>"+bk[i].getJudul()+"</td>"
                             + "<td>"+bk[i].getPenulis()+"</td>"
                             + "<td>"+bk[i].getPenerbit()+"</td>"
                             + "<td>"+bk[i].getTahun_Terbit()+"</td>"
-                            + "<td>"+bk[i].getKetersediaan()+"</td>"
-                            + "</table>");
+                            + "<td>"+bk[i].getKetersediaan()+"</td>");
                 }
             }
+            out.println("</table>");
+            out.println("</body>");
+            out.println("</html>");
         }
     }
 
